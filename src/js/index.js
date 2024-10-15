aa7868f65c937a66f4e77772651d4b7be2ae3472
AndriiNS <andriy.naumov05@gmail.com>
2024-10-15 14:59:41 +0300

    preloader

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import mobileNav from "./modules/mobile-nav.js";
+gsap.registerPlugin(ScrollTrigger);
 mobileNav();
 //========================================================================================================================================================
 
@@ -144,3 +147,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 //========================================================================================================================================================
+//preloader + header
+gsap
+  .timeline({
+    defaults: { ease: "power2.inOut", duration: 1 },
+    onComplete: () => {
+      document.body.style.overflow = "auto";
+      startHeaderAnimation();
+    }
+  })
+  .fromTo(".preloader img", { width: 0, height: 0, opacity: 0 }, { width: "150px", height: "150px", opacity: 1 })
+  .to(".preloader__green", { left: "0%", duration: 3 }, "-=0.5")
+  .fromTo(".preloader__text p:first-child", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 }, "-=2.5")
+  .fromTo(".preloader__text p:last-child", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 }, "-=1.5")
+  .to(".preloader", { opacity: 0, pointerEvents: "none", duration: 0.5, delay: 0.5 })
+  .from(".header", { opacity: 0, y: -50, duration: 1, delay: 0 });
